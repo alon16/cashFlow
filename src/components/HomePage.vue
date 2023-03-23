578c62f1f9ff7b21d6e798d9dbdadbb8cab9587e
Alondra <alondra.ruiz@hotaka.io>
2023-03-23 16:18:05 -0600

    added movements

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -3,15 +3,25 @@
     <template #header>
       <header-page></header-page>
     </template>
+    <template #resume>
+      <resume-page></resume-page>
+    </template>
+    <template #movements>
+      <movements-page></movements-page>
+    </template>
   </layout-page>
 </template>
 <script>
 import LayoutPage from "./LayoutPage.vue";
 import HeaderPage from "./HeaderPage.vue";
+import ResumePage from "./Resume/IndexPage.vue";
+import MovementsPage from "./MovementsPage.vue";
 export default {
   components: {
     LayoutPage,
     HeaderPage,
+    ResumePage,
+    MovementsPage,
   },
 };
 </script>
